4cfdac4978755d82d36ae57ce6c8e25700627627
emdevelopa <gyimahsunday@gmail.com>
2023-09-27 15:15:18 +0100

    Portfolio

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -5,9 +5,14 @@ export default function About() {
     return (
         <>
             <Box marginTop={10}>
-                <Stack alignItems='center' rowGap={4}> 
+                <Stack alignItems='center' rowGap={2}> 
                     <Typography variant="h3" fontWeight={600}>ABOUT ME</Typography>
-                    <Box bgcolor='#7843e9' width={60} height={10} borderRadius={5}></Box>
+                    <Box bgcolor='#7843e9' width={60} height={8} borderRadius={5}></Box>
+                    <Typography variant="p" fontSize={20} >Here you will find more information about me, what I do, and my current skills mostly in terms of programming and technology</Typography>
+                    <Stack direction={"row"} bgcolor='red' width='100%' justifyContent={""}>
+                        <Box><Typography variant="h4" fontWeight={800}>Get to know me!</Typography></Box>
+                        <Box><Typography variant="h4" fontWeight={800}>My Skills</Typography></Box>
+                    </Stack>
                 </Stack>
             </Box>
         </>
