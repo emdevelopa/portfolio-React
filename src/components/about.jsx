382669d0f8dcc2e61ddb6a0667728f9f90d6197e
emdevelopa <gyimahsunday@gmail.com>
2023-09-27 22:46:45 +0100

    Portfolio

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -20,9 +20,9 @@ export default function About() {
                     <Typography variant="h4" fontWeight={600}>ABOUT ME</Typography>
                     <Box bgcolor='#7843e9' width={60} height={8} borderRadius={5}></Box>
                     <Typography variant="p" fontSize={20} >Here you will find more information about me, what I do, and my current skills mostly in terms of programming and technology</Typography>
-                    <Stack direction={"row"} bgcolor='red' width='100%' justifyContent={"space-between"}>
-                        <Box width='100%' bgcolor='blue' paddingX={5}>
-                            <Typography variant="h5" fontWeight={800}>Get to know me!</Typography>
+                    <Stack direction={"row"}  width='100%' justifyContent={"space-between"}>
+                        <Box width='100%' bgcolor='blu' paddingX={5}>
+                            <Typography variant="h5" marginBottom={5} fontWeight={800}>Get to know me!</Typography>
                             <Typography>I'm a Frontend Web Developer building the Front-end of Websites and Web Applications that leads to the success of the overall product. Check out some of my work in the Projects section.</Typography>
                             <br />
                             <Typography>I also like sharing content related to the stuff that I have learned over the years in Web Development so it can help other people of the Dev Community. Feel free to Connect or Follow me on my Linkedin where I post useful content related to Web Development and Programming</Typography><br />
