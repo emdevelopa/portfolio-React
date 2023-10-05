a6ee2555b773bb7ca814d2e56ef0919738155151
emdevelopa <gyimahsunday@gmail.com>
2023-10-05 13:52:38 +0100

    Portfolio

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -9,7 +9,16 @@ export default function Projects() {
                     <Box bgcolor='#7843e9' width={60} height={8} borderRadius={5}></Box>
                     <Typography variant="p" marginBottom={5} fontSize={20} textAlign='center' paddingX={2}>Here you will find some of the personal and clients projects that I created with each project containing its own case study</Typography>
                 </Stack>
-                <Box></Box>
+                <Stack bgcolor='red' direction='row' height={400} justifyContent='space-between' alignItems='center' gap={2} sx={{
+                                '@media (max-width: 600px)': {
+                                    flexDirection:'column'
+                                },
+                            }}>
+                    <Box bgcolor='blue' height={200} width={200}></Box>
+                    <Box bgcolor='blue' height={200} width={200}></Box>
+                    <Box bgcolor='blue' height={200} width={200}></Box>
+                    <Box bgcolor='blue' height={200} width={200}></Box>
+                </Stack>
             </Box>
         </>
     )
